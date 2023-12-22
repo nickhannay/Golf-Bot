4e69181ef3446932a42a919fe75f6a5cbfbad136
nick hannay <nickhannay@outlook.com>
2023-12-21 22:41:54 -0800

    favicon now works in firefox; issues with edge. Working on landing page styling

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const cors = require('cors')
 const express = require('express');
 const path = require('path');
+const favicon = require('serve-favicon');
 
 const indexRouter = require('./src/routes/index');
 const dashboardRouter = require('./src/routes/dashboard');
@@ -10,19 +11,19 @@ const app = express();
 // view engine setup
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'src', 'views'));
-
-
+app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')))
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
-app.use(express.static(path.join(__dirname, 'public')));
+
 
 
 // Routes
 app.use('/', indexRouter);
 app.use('/dashboard', dashboardRouter);
 
+app.use(express.static(path.join(__dirname, 'public')));
 
 
 module.exports = app;
