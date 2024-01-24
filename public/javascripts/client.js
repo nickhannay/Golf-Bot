a2608ff2e0fc7c0a6af05620ad9407d07cd14720
nick hannay <nickhannay@outlook.com>
2024-01-23 17:51:16 -0800

    coverted time to 12 hour and added start hole to teetime-container

--- public/javascripts/client.js
+++ public/javascripts/client.js
@@ -152,9 +152,15 @@ function updateTeeTimes(times){
             console.log(`times subarray = ${sub_times}`)
 
             sub_times.forEach((time) => {
+                const teeTime = convert12hr(time.startTime)
+
                 const container = document.createElement('div')
                 container.classList.add('teetime-container')
-                container.innerText = time.startTime
+                container.innerHTML = `<div class="teetime-TimeLabel"><span>${teeTime}</span><span class="start-hole">Tee ${time.startingTee}</span></div>`
+            
+
+
+
                 row.appendChild(container)
             })
             
@@ -170,3 +176,23 @@ function updateTeeTimes(times){
     }
 }
 
+
+function convert12hr(timeStamp){
+    // time : year-month-dayThour:minute:second
+    const time24hr = timeStamp.split('T')[1]
+
+    const hour_int = parseInt(time24hr.split(':')[0], 10)
+    const minute = time24hr.split(':')[1]
+    let am_pm = 'am'
+    let hour12 = ''
+
+    if(hour_int <= 12){
+        hour12 = hour_int.toString()
+    }
+    else {
+        hour12 = (hour_int % 12 ).toString()
+        am_pm = 'pm'
+    }
+
+    return `${hour12}:${minute} ${am_pm}`
+}
