8d5c47bee78211ae5c14d52f5cc454ae14fe1c4d
nick hannay <nickhannay@outlook.com>
2024-01-25 02:06:21 -0800

    reserve page now displays Course, Holes, Number of Players, Date, Time, and allows for customization of group memebers; working on displaying price

--- src/routes/reserve.js
+++ src/routes/reserve.js
@@ -19,15 +19,50 @@ router.get('/', (req, res) => {
     debug(req.query.teeSheetId)
     debug(`num players: ${req.query.numPlayers}`)
 
+    const timeSlices = req.query.teeTime.split(' ')
+    const teeTime = `${timeSlices[0]} ${timeSlices[1].toUpperCase()}`
+    const teeDate = convertTeeDate(req.query.teeDate)
+
     const template_params = {
         teeSheetId : req.query.teeSheetId, 
         fullName: req.cookies.fullName, 
         numPlayers: req.query.numPlayers,
-        teeTime: req.query.teeTime,
-        teeDate: req.query.teeDate
+        teeTime: teeTime,
+        teeDate: teeDate
     }
     res.render('reserve', template_params)
 })
+
+
+function convertTeeDate(date){
+
+    const months = [
+        'Jan',
+        'Feb',
+        'Mar',
+        'Apr',
+        'May',
+        'Jun',
+        'Jul',
+        'Aug',
+        'Sep',
+        'Oct',
+        'Nov',
+        'Dec'
+    ];
+
+    const dateSlices = date.split('-')
+    const year = dateSlices[0]
+    const month = months[dateSlices[1] - 1 ]
+    const day = dateSlices[2]
+
+
+    
+
+
+    return `${month} ${day}, ${year}`
+
+}
 
 
 module.exports = router
